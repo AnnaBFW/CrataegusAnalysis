37da6a7bc20c20fd7db91c244dccf02d46621a1a
Zöchner Anna <anna.zoechner@bfw.gv.at>
2021-11-29 16:39:30 +0100

    Update PCA_Laevigata_Rhipidophylla_Monogyna.R

--- PCA/PCA_Laevigata_Rhipidophylla_Monogyna.R
+++ PCA/PCA_Laevigata_Rhipidophylla_Monogyna.R
@@ -4,7 +4,7 @@ crataegus_morph <- read.csv("RearrangedTable.csv", na="NA")
 
 # Create Dataframe from .csv File
 # Exclude data about sepals
-df <- crataegus_morph[-c(3,6:8)]
+df <- crataegus_morph[-c(3,5:8)]
 df <- df[complete.cases(df), ] # deletes NAs
 
 # Create subset with wanted species:
@@ -14,14 +14,6 @@ df <- df[complete.cases(df), ] # deletes NAs
 
 df_sub <- subset(df, samp_target == "3" | samp_target == "4" |samp_target == "5")
 
-
-# Create dataframes for each feature
-disk_radius = df_sub[2]
-tot_radius = df_sub[3]
-fr_length = df_sub[4]
-fr_width = df_sub[5]
-fr_pos = df_sub[6]
-styles = df_sub[7]
 
 # PCA
 crat_pca <- prcomp(df_sub[,c(2:4)], center = TRUE,scale. = TRUE)
@@ -35,12 +27,15 @@ install_github('sinhrks/ggfortify')
 
 library(devtools)
 library(ggbiplot)
-library(ggfortify); library(ggplot2)
+library(ggfortify); 
+library(ggplot2)
 
 
 
 autoplot(crat_pca)
 autoplot(crat_pca, data = df_sub, colour = 'samp_target', loadings=TRUE, loadings.label=TRUE)
+
+#index = c("disk_radius", "fr_length", "styles")
 
 
 # Plotting Cluster package
