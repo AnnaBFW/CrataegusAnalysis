3243864d97c6a802f592c8cee1b848244f71707d
Zöchner Anna <anna.zoechner@bfw.gv.at>
2021-11-29 16:39:34 +0100

    Update Morph_Ploidy.R

--- Ploidy_data/Morph_Ploidy.R
+++ Ploidy_data/Morph_Ploidy.R
@@ -8,11 +8,11 @@ library(dplyr)
 
 ## Data preparation ##
 
-setwd("C:/Users/anna.zoechner/OneDrive - Bundesforschungszentrum fuer Wald/Dokumente/GitHub/Ploidy_data")
+setwd("C:/Users/anna.zoechner/OneDrive - Bundesforschungszentrum fuer Wald/Dokumente/GitHub/CrataegusAnalysis/Ploidy_data")
 
 
 # Read in table with morphometric data
-crataegus_morph <- read.csv("RearrangedTable.csv", na="NA")
+crataegus_morph <- read.csv("RearrangedTable_samp_names.csv", na="NA")
 
 
 # Read in table with ploidy data
@@ -34,7 +34,7 @@ df_sub <- subset(df, samp_target == "3" | samp_target == "4" | samp_target == "5
 
 
 # Exclude data about sepals
-df_sub <- df_sub[-c(8,9,11)]
+df_sub <- df_sub[-c(7,8,10,11)]
 df_sub <- df_sub[complete.cases(df_sub), ] # deletes NAs
 
 
@@ -43,15 +43,17 @@ df_sub_L <- subset(df_sub, samp_target == "3")
 df_sub_R <- subset(df_sub, samp_target == "4")
 df_sub_M <- subset(df_sub, samp_target == "5")
 
-
+summary(df_sub_L)
+summary(df_sub_R)
+summary(df_sub_M)
 
 ## Exploratory Data Analysis ## 
 
 # Boxplots for each sepcies
 par(mfrow=c(1,3),mar=c(6,3,2,1))
-boxplot(df_sub_L[,3:9], main="Laevigata",ylim = c(0,14),las=2)
-boxplot(df_sub_R[,3:9], main="Rhipidophylla",ylim = c(0,14),las=2)
-boxplot(df_sub_M[,3:9], main="Monogyna",ylim = c(0,14),las=2)
+boxplot(df_sub_L[,2:8], main="Laevigata",ylim = c(0,14),las=2)
+boxplot(df_sub_R[,2:8], main="Rhipidophylla",ylim = c(0,14),las=2)
+boxplot(df_sub_M[,2:8], main="Monogyna",ylim = c(0,14),las=2)
 
 # Histogram for ploidy for each species
 hist(df_sub_L$ploidy,breaks=seq(0,4,l=17),xlim=c(0,4),ylim=c(0,250))
@@ -60,12 +62,12 @@ hist(df_sub_M$ploidy,breaks=seq(0,4,l=17),xlim=c(0,4),ylim=c(0,250))
 
 # Correlations between Variables
 par(mfrow=c(1,1))
-corr <- cor(df_sub[,3:9])
+corr <- cor(df_sub[,2:8])
 corr_matrix <- round(corr, 3)
 
 write.table(corr_matrix,"Correlation_matrix.csv", row.names = FALSE)
 
-pairs(df_sub[,3:9])
+pairs(df_sub[,2:8])
 
 # Are the (visual) correlations different for each class?
 pairs(df_sub[,3:9],col=df_sub[,2],oma=c(4,4,6,12))
@@ -148,3 +150,7 @@ autoplot(crat_pca_clean, data = df_pca, colour = 'samp_target' )
 # PCA with selected features but without ploidy
 autoplot(crat_pca_clean_withoutploidy, data = df_pca_withoutploidy, colour = 'samp_target' )
 
+
+mean_vals_L <- apply(df_sub_L, MARGIN = 2, mean)
+mean_vals_M <- apply(df_sub_M, MARGIN = 2, mean)
+mean_vals_R <- apply(df_sub_R, MARGIN = 2, mean)
