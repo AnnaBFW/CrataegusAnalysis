cefcac0ddd8082f08bfff2501fb8e437c1aa32a1
Zöchner Anna <anna.zoechner@bfw.gv.at>
2021-11-29 16:39:16 +0100

    Update Clustering_Laevigata_Monogyna_Rhipidophylla.R

--- ClusteringInR/Clustering_Laevigata_Monogyna_Rhipidophylla.R
+++ ClusteringInR/Clustering_Laevigata_Monogyna_Rhipidophylla.R
@@ -3,6 +3,13 @@ setwd("C:/Users/anna.zoechner/OneDrive - Bundesforschungszentrum fuer Wald/Dokum
 crataegus_morph <- read.csv("RearrangedTable.csv", na="NA")
 
 # Create Dataframe from .csv File
+
+# Change next lines depending on if you want to include speal data or not
+# Either comment line 10 (exclude sepals) or 14 & 15 (include sepals)
+
+# keep all
+df <- crataegus_morph[complete.cases(crataegus_morph), ]
+
 # Exclude data about sepals
 df <- crataegus_morph[-c(6:8)]
 df <- df[complete.cases(df), ] # deletes NAs
@@ -16,15 +23,6 @@ df_sub <- subset(df, samp_target == "3" | samp_target == "4" |samp_target == "5"
 
 # Standardization
 df_sub <- scale(df_sub) # standardize variables
-
-
-# Create dataframes for each feature
-disk_radius = df_sub[2]
-tot_radius = df_sub[3]
-fr_length = df_sub[4]
-fr_width = df_sub[5]
-fr_pos = df_sub[6]
-styles = df_sub[7]
 
 # Determine number of clusters
 wss <- (nrow(df_sub)-1)*sum(apply(df_sub,2,var))
@@ -42,7 +40,7 @@ aggregate(df_sub,by=list(fit$cluster),FUN=mean)
 df_sub <- data.frame(df_sub, fit$cluster)
 
 library(cluster)
-clusplot(df_sub, fit$cluster, color=TRUE, shade=TRUE,lines=0)
+clusplot(df_sub, fit$cluster, color=TRUE, shade=TRUE,lines=0, main = "KMeans Cluster")
 
 # Centroid Plot against 1st 2 discriminant functions
 install.packages("fpc")
